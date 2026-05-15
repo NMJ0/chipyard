e96caa25acfb5cbab7f553a4255a0eb262994b3a
NMJ0 <noelmanuel.in@gmail.com>
2026-05-15 10:24:58 +0530

    included uart-tsi support for vc707

--- fpga/src/main/scala/vc707/Configs.scala
+++ fpga/src/main/scala/vc707/Configs.scala
@@ -1,6 +1,6 @@
-package chipyard.fpga.vc707
 
-import sys.process._
+// See LICENSE for license details.
+package chipyard.fpga.vc707
 
 import org.chipsalliance.cde.config.{Config, Parameters}
 import freechips.rocketchip.subsystem.{SystemBusKey, PeripheryBusKey, ControlBusKey, ExtMem}
@@ -10,9 +10,6 @@ import freechips.rocketchip.diplomacy.{RegionType, AddressSet}
 import freechips.rocketchip.resources.{DTSModel, DTSTimebase}
 import freechips.rocketchip.util.{SystemFileName}
 
-import sifive.blocks.devices.spi.{PeripherySPIKey, SPIParams}
-import sifive.blocks.devices.uart.{PeripheryUARTKey, UARTParams}
-
 import sifive.fpgashells.shell.{DesignKey}
 import sifive.fpgashells.shell.xilinx.{VC7074GDDRSize}
 
@@ -21,58 +18,25 @@ import testchipip.serdes.{SerialTLKey}
 import chipyard.{BuildSystem, ExtTLMem}
 import chipyard.harness._
 
-class WithDefaultPeripherals extends Config((site, here, up) => {
-  case PeripheryUARTKey => List(UARTParams(address = BigInt(0x64000000L)))
-  case PeripherySPIKey => List(SPIParams(rAddress = BigInt(0x64001000L)))
-})
+import gemmini.DefaultGemminiConfig
 
+// ------------------------------------------------------------
+// System tweaks (bootrom, memory, etc.)
+// ------------------------------------------------------------
 class WithSystemModifications extends Config((site, here, up) => {
-  case DTSTimebase => BigInt{(1e6).toLong}
+  case DTSTimebase => BigInt((1e6).toLong)
   case BootROMLocated(x) => up(BootROMLocated(x), site).map { p =>
-    // invoke makefile for sdboot
-    val freqMHz = (site(SystemBusKey).dtsFrequency.get / (1000 * 1000)).toLong
-    val make = s"make -C fpga/src/main/resources/vc707/sdboot PBUS_CLK=${freqMHz} bin"
-    require (make.! == 0, "Failed to build bootrom")
-    p.copy(hang = 0x10000, contentFileName = SystemFileName(s"./fpga/src/main/resources/vc707/sdboot/build/sdboot.bin"))
+    p.copy(hang = 0x10000)
   }
-  case ExtMem => up(ExtMem, site).map(x => x.copy(master = x.master.copy(size = site(VC7074GDDRSize)))) // set extmem to DDR size (note the size)
-  case SerialTLKey => Nil // remove serialized tl port
+  case ExtMem => up(ExtMem, site).map(x =>
+    x.copy(master = x.master.copy(size = site(VC7074GDDRSize))))
+  case SerialTLKey => Nil
 })
 
-class WithVC707Tweaks extends Config (
-  // clocking
-  new chipyard.harness.WithAllClocksFromHarnessClockInstantiator ++
-  new chipyard.clocking.WithPassthroughClockGenerator ++
-  new chipyard.config.WithUniformBusFrequencies(50.0) ++
-
-  new chipyard.harness.WithHarnessBinderClockFreqMHz(50) ++
-  new WithFPGAFrequency(50) ++ // default 50MHz freq
-  // harness binders
-  new chipyard.harness.WithAllClocksFromHarnessClockInstantiator ++
-  new WithVC707UARTHarnessBinder ++
-  new WithVC707SPISDCardHarnessBinder ++
-  new WithVC707DDRMemHarnessBinder ++
-  // other configuration
-  new WithDefaultPeripherals ++
-  new chipyard.config.WithTLBackingMemory ++ // use TL backing memory
-  new WithSystemModifications ++ // setup busses, use sdboot bootrom, setup ext. mem. size
-  new chipyard.config.WithNoDebug ++ // remove debug module
-  new freechips.rocketchip.subsystem.WithoutTLMonitors ++
-  new freechips.rocketchip.subsystem.WithNMemoryChannels(1)
-)
-
-class RocketVC707Config extends Config (
-  new WithVC707Tweaks ++
-  new chipyard.RocketConfig
-)
-
-class BoomVC707Config extends Config (
-  new WithFPGAFrequency(50) ++
-  new WithVC707Tweaks ++
-  new chipyard.MegaBoomV3Config
-)
-
-class WithFPGAFrequency(fMHz: Double) extends Config (
+// ------------------------------------------------------------
+// Parameterized FPGA frequency config
+// ------------------------------------------------------------
+class WithFPGAFrequency(fMHz: Double) extends Config(
   new chipyard.config.WithPeripheryBusFrequency(fMHz) ++
   new chipyard.config.WithMemoryBusFrequency(fMHz) ++
   new chipyard.config.WithSystemBusFrequency(fMHz) ++
@@ -80,7 +44,88 @@ class WithFPGAFrequency(fMHz: Double) extends Config (
   new chipyard.config.WithFrontBusFrequency(fMHz)
 )
 
-class WithFPGAFreq25MHz extends WithFPGAFrequency(25)
-class WithFPGAFreq50MHz extends WithFPGAFrequency(50)
-class WithFPGAFreq75MHz extends WithFPGAFrequency(75)
+class WithFPGAFreq20MHz  extends WithFPGAFrequency(20)
+class WithFPGAFreq25MHz  extends WithFPGAFrequency(25)
+class WithFPGAFreq50MHz  extends WithFPGAFrequency(50)
+class WithFPGAFreq75MHz  extends WithFPGAFrequency(75)
 class WithFPGAFreq100MHz extends WithFPGAFrequency(100)
+
+// ------------------------------------------------------------
+// VC707 Tweaks (NOW PARAMETERIZED)
+// ------------------------------------------------------------
+class WithVC707Tweaks(freqMHz: Double = 50) extends Config(
+
+  // ---- clocking -------------------------------------------------------
+  new chipyard.harness.WithAllClocksFromHarnessClockInstantiator ++
+  new chipyard.clocking.WithPassthroughClockGenerator ++
+  new chipyard.config.WithUniformBusFrequencies(freqMHz) ++
+  new chipyard.harness.WithHarnessBinderClockFreqMHz(freqMHz) ++
+  new WithFPGAFrequency(freqMHz) ++
+
+  // ---- harness binders ------------------------------------------------
+  new WithVC707UARTTSIHarnessBinder ++
+  new WithVC707DDRMemHarnessBinder ++
+
+  // ---- UART-TSI client ------------------------------------------------
+  new testchipip.tsi.WithUARTTSIClient ++
+  new chipyard.harness.WithSerialTLTiedOff ++
+
+  // ---- chip-level config ----------------------------------------------
+  new chipyard.config.WithNoUART ++
+  new chipyard.config.WithTLBackingMemory ++
+  new WithSystemModifications ++
+  new chipyard.config.WithNoDebug ++
+  new freechips.rocketchip.subsystem.WithoutTLMonitors ++
+  new freechips.rocketchip.subsystem.WithNMemoryChannels(1)
+)
+
+// ------------------------------------------------------------
+// Base configs (parameterized)
+// ------------------------------------------------------------
+class RocketVC707Config extends Config(
+  new WithVC707Tweaks(50) ++
+  new chipyard.RocketConfig
+)
+
+class RocketGemminiVC707Config extends Config(
+  new DefaultGemminiConfig ++
+  new WithVC707Tweaks(50) ++
+  new chipyard.config.WithBroadcastManager ++
+  new chipyard.RocketConfig
+)
+
+class BoomVC707Config extends Config(
+  new WithVC707Tweaks(50) ++
+  new chipyard.MegaBoomV3Config
+)
+
+// ------------------------------------------------------------
+// NEW: 25 MHz configs
+// ------------------------------------------------------------
+class RocketVC70725MHzConfig extends Config(
+  new WithVC707Tweaks(25) ++
+  new chipyard.RocketConfig
+)
+
+class RocketGemminiVC70725MHzConfig extends Config(
+  new DefaultGemminiConfig ++
+  new WithVC707Tweaks(25) ++
+  new chipyard.config.WithBroadcastManager ++
+  new chipyard.RocketConfig
+)
+
+// ------------------------------------------------------------
+// NEW: 20 MHz configs
+// ------------------------------------------------------------
+class RocketVC70720MHzConfig extends Config(
+  new WithVC707Tweaks(20) ++
+  new chipyard.RocketConfig
+)
+
+class RocketGemminiVC70720MHzConfig extends Config(
+  new DefaultGemminiConfig ++
+  new WithVC707Tweaks(20) ++
+  new chipyard.config.WithBroadcastManager ++
+  new chipyard.RocketConfig
+)
+
